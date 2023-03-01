7678ad796c4157cafb643d507853241cca2b5bda
Carl <carl.ostrand@gmail.com>
2023-03-01 18:52:23 +0100

    refactor: Stared to use AttiniRuntimeVariables to en environment name

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import os
 
 import aws_cdk as cdk
 
+from attini_cdk import AttiniRuntimeVariables as RuntimeVars
+
 from infrastructure.cdk_project_stack import CdkProjectStack
 
 app = cdk.App()
 
-if "ENV" not in os.environ:
-    os.environ["ENV"]="dev"
+if RuntimeVars.ENVIRONMENT not in os.environ:
+    os.environ[RuntimeVars.ENVIRONMENT] = "dev"
 
 
 hello_world_stack = CdkProjectStack(app, "hello-world-stack",
-                                    stack_name=f"{os.environ['ENV']}-hello-world-stack",
+                                    stack_name=f"{os.environ[RuntimeVars.ENVIRONMENT]}-hello-world-stack",
                                     env=cdk.Environment(
                                         account=os.getenv("CDK_DEFAULT_ACCOUNT"),
                                         region=os.environ["CDK_DEFAULT_REGION"]
